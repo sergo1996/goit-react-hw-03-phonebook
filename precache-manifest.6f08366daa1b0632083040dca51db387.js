c5f6f271e914b58d6171ade647f593caeaa6840f
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-09-19 11:09:27 +0300

    Updates

---
+++ precache-manifest.6f08366daa1b0632083040dca51db387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec05f529806936ac3e51ad1be8ea7c4b",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "e089ebc79593a8d16ced",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.1cdf1d7c.chunk.css"
+  },
+  {
+    "revision": "26c24f57d8246e0e00b0",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.905fd19b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.905fd19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e089ebc79593a8d16ced",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.171fb509.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
